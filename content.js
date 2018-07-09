8c1ac7e722152c9055893164a61fa844dc1a571a
wheatup <791156900@qq.com>
2018-07-09 23:38:28 +0900

    ???

--- content.js
+++ content.js
@@ -2,6 +2,6 @@ var logos = document.body.getElementsByClassName('logo');
 if(logos){
 	var logo = logos[0].children[0].children[0];
 	var imgURL = chrome.extension.getURL("images/logo-2015-night.gif");
-	var background = `url("${imgURL}") no-repeat scroll 0 0 transparent`;
+	var background = 'url("' + imgURL + '") no-repeat scroll 0 0 transparent !important';
 	logo.style.background = background;
 }
